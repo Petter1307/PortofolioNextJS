65677af273c9c8c316d833d79ffc76ef9c249f41
Petter1307 <petrealupu75@gmail.com>
2022-08-16 18:41:47 +0300

    Finished getStaticProps implementation.

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,71 +1,62 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable react/prop-types */
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable no-plusplus */
-import { useState, useEffect, useCallback } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import axios from 'axios';
 import { ProjectCard, HElement, PageLayout } from '../../components';
-import { getUserData } from '../../api/github';
-import { Repo, Data, RepoItems } from '../../types/types';
+import { Repo } from '../../types/types';
 
-const Home = () => {
-  const getUserRepo = useCallback(async () => {
-    try {
-      const response = await axios.get(
-        'https://api.github.com/users/petter1307/repos'
-      );
-      const listRepos = response.data;
-      const array: RepoItems = [];
-      for (let i = 0; i < listRepos.length; i++) {
-        array.push({
-          id: listRepos[i].id,
-          name: listRepos[i].name,
-          url: listRepos[i].html_url,
-          updated_at: listRepos[i].updated_at,
-        });
-      }
-      console.log(array);
-      return array;
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  }, []);
-  const [profile, setProfile] = useState<Data>();
-  const [repos, setRepos] = useState<RepoItems>();
-  useEffect(() => {
-    getUserData().then(resp => {
-      setProfile(prof => {
-        return { ...prof, ...resp };
-      });
-    });
-    getUserRepo().then(data => {
-      setRepos(old => data);
-    });
-  }, []);
-
+// export async function getStaticProps() {
+//   const profileRes = await fetch('https://api.github.com/users/petter1307 ');
+//   const profileData = profileRes.json();
+//   const repoRes = await fetch('https://api.github.com/users/petter1307/repos');
+//   const reposData = repoRes.json();
+//   return {
+//     props: {
+//       profileData,
+//       repoList: reposData,
+//     },
+//   };
+// }
+// @ts-ignore
+const Home = ({ profileData, reposList }) => {
   return (
     <>
       <HElement value="home" />
       <PageLayout name="home" title="Home">
         <div className="page_home-summary">
           <div className="page_home-summary-profile">
-            <img alt="place" src={profile?.avatar} width="100" height="100" />
+            <img
+              alt="place"
+              src={profileData?.avatar}
+              width="100"
+              height="100"
+            />
             <div className="page_home-summary-profile-text">
               <h1>
-                {profile?.name}
-                <a href={profile?.html_url} target="#">
+                {profileData?.name}
+                <a href={profileData?.html_url} target="#">
                   <FontAwesomeIcon icon={faGithub} />
                 </a>
               </h1>
               <hr />
-              <h3>{`Followers:  ${profile?.followers}`}</h3>
-              <h3>Company: {profile?.company}</h3>
+              <h3>{`Followers:  ${profileData?.followers}`}</h3>
+              <h3>Company: {profileData?.company}</h3>
+              <button
+                onClick={() => {
+                  console.log(reposList);
+                }}
+                type="submit"
+              >
+                XD
+              </button>
             </div>
           </div>
           <div className="page_home-summary-projects">
             <h1>Projects</h1>
-            {repos?.map((repo: Repo) => (
+            {reposList?.map((repo: Repo) => (
               <ProjectCard
                 title={repo.name}
                 link={repo.url}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,59 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-plusplus */
+import axios from 'axios';
+import { Data, RepoItems } from 'types/types';
 import Home from './home/index';
 import { DataProvider } from '../providers/DataProvider';
 
-const IndexPage = () => {
+// @ts-ignore
+const IndexPage = ({ profileData, reposList }) => {
   return (
     <DataProvider>
-      <Home />
+      <Home profileData={profileData} reposList={reposList} />
     </DataProvider>
   );
 };
 export default IndexPage;
+
+export async function getStaticProps() {
+  console.log('This was called');
+  try {
+    const response = await axios.get('https://api.github.com/users/petter1307');
+    const returnedData: Data = {
+      name: response.data.name,
+      company: response.data.company,
+      followers: response.data.followers,
+      html_url: response.data.html_url,
+      avatar: response.data.avatar_url,
+    };
+    const responseRepo = await axios.get(
+      'https://api.github.com/users/petter1307/repos'
+    );
+    const listRepos = responseRepo.data;
+    const repoDataList: RepoItems = [];
+    for (let i = 0; i < listRepos.length; i++) {
+      repoDataList.push({
+        id: listRepos[i].id,
+        name: listRepos[i].name,
+        url: listRepos[i].html_url,
+        updated_at: listRepos[i].updated_at,
+      });
+    }
+
+    return {
+      props: {
+        profileData: returnedData,
+        reposList: repoDataList,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        profileData: {},
+        reposList: [],
+      },
+    };
+  }
+}
